39ae5090e918a039692f62d3027493b49abd39df
Martin Crabtree <mcrabtr33@gmail.com>
2020-01-05 14:48:12 -0500

    added login and overlord component

---
+++ adventurehelper/src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Login=(props)=>{
+
+    return(<div> this is the login component</div>)
+}
+export default Login;
---
+++ adventurehelper/src/components/Mainpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Mainpage=(props)=>{
+
+
+    return(
+        <div> this is the mainpage component</div>
+    )
+}
+
+export default Mainpage;
